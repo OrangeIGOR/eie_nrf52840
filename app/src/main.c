375a3a231434470b3b6f58e93b8a16cd89e1daed
OrangeIGOR <oisukyar@gmail.com>
2025-10-23 20:17:18 -0600

    ADVANCED PASSWORD

--- app/src/main.c
+++ app/src/main.c
@@ -27,48 +27,116 @@ int main(void) {
   uint32_t password_mask = 0;
   uint32_t password_key = 0x00421421;
   uint8_t  num_in = 0;
+  uint8_t  intialize = 0;
+  uint8_t  timeout = 0;
+  uint32_t mode_count = 0;
   LED_set(LED0, LED_ON);
+  LED_set(LED1, LED_ON);
   while(1) {
-
-    if(BTN_check_clear_pressed(BTN0))
+    if(intialize == 0)
     {
-      password_mask = password_mask | (0x1 << 4*num_in);
-      num_in++;
-    }
-    if(BTN_check_clear_pressed(BTN1))
-    {
-      password_mask = password_mask | (0x2 << 4*num_in);
-      num_in++;
-    }
-    if(BTN_check_clear_pressed(BTN2))
-    {
-      password_mask = password_mask | (0x4 << 4*num_in);
-      num_in++;
-    }
-    if(num_in > 8){
-      printk("Incorrect!\n");
-      num_in = 0;
-      password_mask = 0;
-    }
-
-    if(BTN_check_clear_pressed(BTN3))
-    {
-      if(password_mask == password_key){
-        printk("Correct!\n");
-        LED_set(LED0, LED_OFF);
-        while(0 == BTN_check_clear_pressed(BTN0) ||
-                    BTN_check_clear_pressed(BTN1) ||
-                    BTN_check_clear_pressed(BTN2) ||
-                    BTN_check_clear_pressed(BTN3)){}
-        LED_set(LED0, LED_ON);
-        printk("LOCKED!\n");
+      if(BTN_check_clear_pressed(BTN3))
+      {
+        LED_set(LED1, LED_OFF);
+        LED_set(LED2, LED_ON);
+        intialize = 2;
+        password_key = 0;
+        printk("PASSWORD CHANGE!\n");
       }
-      else{
+
+      if(timeout)
+      {
+        LED_set(LED1, LED_OFF);
+        LED_set(LED2, LED_OFF);
+        intialize = 1;
+        timeout = 0;
+        printk("LOCKED, ENTER PASSWORD!\n");
+      }
+
+      if(mode_count > 3000)
+      {
+        timeout = 1;
+        mode_count = 0;
+      }
+
+      mode_count++;
+      k_msleep(1);
+    }
+
+
+    if(intialize == 1){
+      if(BTN_check_clear_pressed(BTN0))
+      {
+        password_mask = password_mask | (0x1 << 4*num_in);
+        num_in++;
+      }
+      if(BTN_check_clear_pressed(BTN1))
+      {
+        password_mask = password_mask | (0x2 << 4*num_in);
+        num_in++;
+      }
+      if(BTN_check_clear_pressed(BTN2))
+      {
+        password_mask = password_mask | (0x4 << 4*num_in);
+        num_in++;
+      }
+      if(num_in > 8){
         printk("Incorrect!\n");
+        num_in = 0;
+        password_mask = 0;
       }
-      num_in = 0;
-      password_mask = 0;
+
+      if(BTN_check_clear_pressed(BTN3))
+      {
+        if(password_mask == password_key){
+          printk("Correct!\n");
+          LED_set(LED0, LED_OFF);
+          while(0 < BTN_check_clear_pressed(BTN0) ||
+                      BTN_check_clear_pressed(BTN1) ||
+                      BTN_check_clear_pressed(BTN2) ||
+                      BTN_check_clear_pressed(BTN3)){}
+          LED_set(LED0, LED_ON);
+          printk("WAIT TO LOCK, OR BUTTON3 FOR PASSCHANGE!\n");
+          intialize = 0;
+        }
+        else{
+          printk("Incorrect!\n");
+        }
+        num_in = 0;
+        password_mask = 0;
+      }
     }
+
+    if(intialize == 2){
+      
+      if(BTN_check_clear_pressed(BTN0))
+      {
+        password_key = password_key | (0x1 << 4*num_in);
+        num_in++;
+      }
+      if(BTN_check_clear_pressed(BTN1))
+      {
+        password_key = password_key | (0x2 << 4*num_in);
+        num_in++;
+      }
+      if(BTN_check_clear_pressed(BTN2))
+      {
+        password_key = password_key | (0x4 << 4*num_in);
+        num_in++;
+      }
+      if(num_in > 8){
+        printk("Max Password Size!\n");
+        while(!BTN_check_pressed(BTN3)){}
+      }
+
+      if(BTN_check_clear_pressed(BTN3))
+      {
+        num_in = 0;
+        intialize = 1;
+        printk("NEW PASSWORD SAVED, LOCKED!\n");
+      }
+    }
+      
 
 
   }
