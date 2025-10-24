998c374247d89726109e4d3952d11d889e043735
OrangeIGOR <oisukyar@gmail.com>
2025-10-23 19:37:31 -0600

    LED COUTNER

--- app/src/main.c
+++ app/src/main.c
@@ -8,6 +8,7 @@
 #include <zephyr/sys/printk.h>
 #include <inttypes.h>
 #include <BTN.h>
+#include <LED.h>
 
 #define SLEEP_TIME_MS 1000
 
@@ -23,8 +24,37 @@ int main(void) {
   if (0 > LED_init()) {
     return 0;
   }
-
+  uint8_t led_mask = 0;
   while(1) {
+    if((led_mask & 0b0001) > 0){
+      LED_set(LED0, LED_ON);}
+    else if((led_mask & 0b0001) == 0)
+      LED_set(LED0, LED_OFF);
+      
+    if((led_mask & 0b0010) > 0){
+      LED_set(LED1, LED_ON);}
+    else if((led_mask & 0b0010) == 0)
+      LED_set(LED1, LED_OFF);
+
+    if((led_mask & 0b0100) > 0){
+      LED_set(LED2, LED_ON);}
+    else if((led_mask & 0b0100) == 0)
+      LED_set(LED2, LED_OFF);
+
+    if((led_mask & 0b1000) > 0){
+      LED_set(LED3, LED_ON);}
+    else if((led_mask & 0b1000) == 0)
+      LED_set(LED3, LED_OFF);
+
+    if(BTN_check_clear_pressed(BTN0)){
+      led_mask++;
+      printk("\n%d\n", led_mask);
+    }
+      
+
+    if(led_mask == 16)
+      led_mask = 0;
+
 
   }
 	return 0;
